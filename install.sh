affd487fd209321ec2720971a44ef22aa04a6089
manas140 <manasbhosale.14@gmail.com>
2021-12-12 08:25:33 +0000

    termux*

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ case ${os} in
   Android) font_dest="$HOME/.termux/fonts/."
     cp $program /usr/local/bin/$program && printf "${cg}[*] $program Installed\n${cr}";;
   *) font_dest="$HOME/.fonts/."
-    sudo cp $program /usr/local/bin/$program && printf "${cg}[*] $program Installed\n${cr}";;
+    sudo cp $program /bin/$program && printf "${cg}[*] $program Installed\n${cr}";;
 esac
+mkdir -p $font_dest
 cp -r ttf-material-design-icons/* $font_dest && printf "${cg}[*] Font's Installed\n${cr}"
