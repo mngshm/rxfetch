80a3ade69edc95734b320419cbf125d5fb66ea66
manas140 <manasbhosale.14@gmail.com>
2021-12-12 07:15:13 +0000

    install.sh again..

--- install.sh
+++ install.sh
@@ -2,15 +2,12 @@
 
 cr="\033[1;31m"
 cg="\033[1;32m"
-cb="\033[1;34m"
 program="rxfetch"
 printf "${cr}"
-
-case $1 in 
-  *u*) sudo rm -r /usr/local/bin/$program && printf "${cg}[*] $program Uninstalled\n${cr}";;
-  *i*) cp -r ttf-material-design-icons/* $HOME/.fonts/ && printf "${cg}[*] Font's Installed\n${cr}"
-    sudo cp $program /usr/local/bin/$program && printf "${cg}[*] $program Installed\n${cr}";;
-  *) printf "${cg}Usage: ./install.sh ${cb}[i|u]${cb}
-    i:${cg} Install
-    ${cb}u:${cg} Uninstall\n${cr}"
+os=$(uname -o)
+case ${os} in
+  Android) font_dest="$HOME/.termux/fonts/.";;
+  *) font_dest="$HOME/.fonts/.";;
 esac
+cp -r ttf-material-design-icons/* $font_dest && printf "${cg}[*] Font's Installed\n${cr}"
+sudo cp $program /usr/local/bin/$program && printf "${cg}[*] $program Installed\n${cr}"
